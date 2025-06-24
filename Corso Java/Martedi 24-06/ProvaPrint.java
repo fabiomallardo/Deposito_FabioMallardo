e3e51a3d1ed164213e769e183e72b45cfa946370
Fabio Mallrdo <fabio.mallardo16@gmail.com>
2025-06-24 17:51:08 +0200

    Add files via upload

---
+++ Corso Java/Martedi 24-06/ProvaPrint.java
@@ -0,0 +1,25 @@
+/*
+ * Creazione classe ProvaPrint
+ */
+public class ProvaPrint {
+    public static void main (String[] args){
+        //Sto stampando Hello World
+        System.out.println("Hello World!");
+        // Sto stampando TUTTO OK 
+        System.out.println("TUTTO OK");
+        //Sto stampando Hello World!
+        System.out.print("Hello World! ");
+        //Sto stampando I will print on the same line
+        System.out.print("I will print on the same line.");
+        //Sto stampando 3
+        System.out.println(3);
+        //Sto stampando 358
+        System.out.println(358);
+        //Sto stampando 50000
+        System.out.println(50000);
+        //Sto stampando 3 + 3
+        System.out.println(3 + 3);
+        //Sto stampando 3 * 2
+        System.out.println(3 * 2);
+    }
+}
