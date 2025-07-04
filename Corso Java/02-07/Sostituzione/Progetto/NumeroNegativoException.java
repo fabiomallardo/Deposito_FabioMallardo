468aade3d03e9c192fd6c6e274b77751239c9143
Fabio Mallardo <fabio.mallardo16@gmail.com>
2025-07-04 18:05:37 +0200

    Delete Corso Java directory

--- Corso Java/02-07/Sostituzione/Progetto/NumeroNegativoException.java
+++
@@ -1,8 +0,0 @@
-package Progetto;
-
-public class NumeroNegativoException extends Exception {
-    public NumeroNegativoException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
-
