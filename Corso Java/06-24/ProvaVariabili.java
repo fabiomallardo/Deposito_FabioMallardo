0fb9815ae4459efe868f3235b44d7d46c6e540d2
Fabio Mallardo <fabio.mallardo16@gmail.com>
2025-07-10 18:00:43 +0200

    Delete Corso Java/06-24 directory

--- Corso Java/06-24/ProvaVariabili.java
+++
@@ -1,8 +0,0 @@
-public class ProvaVariabili {
-    public static void main (String[] args) {
-        String pippo = "Fabio";
-        System.out.print("Hi, ");
-        System.out.print(pippo);
-        System.out.println(". How are you?");
-    }
-}
