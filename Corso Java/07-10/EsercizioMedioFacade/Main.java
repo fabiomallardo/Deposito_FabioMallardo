deaf2a4026e153e364eea877c5e62f363d7c685a
Fabio Mallardo <fabio.mallardo16@gmail.com>
2025-07-10 10:52:14 +0200

    Add files via upload

---
+++ Corso Java/07-10/EsercizioMedioFacade/Main.java
@@ -0,0 +1,14 @@
+package EsercizioMedioFacade;
+public class Main {
+    public static void main(String[] args) {
+        // Ottieni l'istanza singleton del Facade
+        FacadeSingleton pc = FacadeSingleton.getIstanza();
+
+        // Registra servizi come observer
+        pc.getSistemaOperativo().registraServizio(new Antivirus());
+        pc.getSistemaOperativo().registraServizio(new GestoreUpdate());
+
+        // Avvia il computer
+        pc.accendiComputer();
+    }
+}
