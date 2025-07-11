99a42cb35d9bce6f608980aa300db2ee50dadc2f
Fabio Mallardo <fabio.mallardo16@gmail.com>
2025-07-11 12:49:47 +0200

    Delete Corso Java/EsercizioFinaleTerzaSettimana directory

--- Corso Java/EsercizioFinaleTerzaSettimana/observer/Observer.java
+++
@@ -1,8 +0,0 @@
-package observer;
-
-// Interfaccia che rappresenta chi riceve le notifiche
-public interface Observer {
-
-    // Metodo che viene chiamato quando c'è una nuova notifica
-    void aggiorna(String messaggio);
-}
